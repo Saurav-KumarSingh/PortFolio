94775258eb4c6290e109dbe043eda1fbc766141b
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-06-21 10:25:43 +0530

    adding links of insta and github etc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
             <div className="mt-8 md:w-[50vw] border-t border-gray-800 p-2 pt-8 text-center m-auto">
 
                 <span className="inline-flex">
-                    <a className="dark:text-white text-black text-xl">
+                    <a href='https://www.linkedin.com/in/sauravkumarsingh1688' className="dark:text-white text-black text-xl">
                         <IoLogoLinkedin />
                     </a>
-                    <a className="ml-4 dark:text-white text-black text-xl">
+                    <a href='https://github.com/Saurav-KumarSingh' className="ml-4 dark:text-white text-black text-xl">
                         <FaGithub />
                     </a>
-                    <a className="ml-4 dark:text-white text-black text-xl">
+                    <a href='https://www.instagram.com/saurav_singh_2999?igsh=cmRsY2NiczYyOWJ3' className="ml-4 dark:text-white text-black text-xl">
                         <FaInstagram />
                     </a>
                 </span>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation'
-
-import { Link } from 'react-router-dom'
 const Hero = () => {
 
   return (
